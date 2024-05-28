be70533991faf8f70ee08edca1b1f7e07546e09b
Atul Sharma <87241497+atul120212@users.noreply.github.com>
2024-05-28 14:43:09 +0530

    Add files via upload

---
+++ logout.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Security;  
+
+namespace TestGridView
+{
+    public partial class logout : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                if (Session["id"] == null)
+                {
+                    Response.Redirect("Login.aspx");
+                }
+                Session.Clear();
+                Session.Abandon();
+                FormsAuthentication.SignOut();
+                var authCookie = new HttpCookie(FormsAuthentication.FormsCookieName, string.Empty) { Expires = DateTime.Now.AddYears(-1) };
+                Response.Cookies.Add(authCookie);
+                FormsAuthentication.RedirectToLoginPage(); 
+                Session.Remove("username");
+                Session.Remove("password");
+                Response.Redirect("~/Login.aspx");
+            }
+        }
+    }
+}
